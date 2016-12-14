2053a3def4ddbfbedba723452dbb75367714e836
Martin <tepira@tepira.com>
2016-12-14 22:46:55 +0100

    - refactor send_request method - added tests options

--- lib/thepay/gateway.rb
+++ lib/thepay/gateway.rb
@@ -9,17 +9,17 @@ module ThePay
                 :customer_email, :return_url, :back_to_eshop_url, :merchant_specific_symbol
 
     def initialize(options = {})
-      @merchant_id              = options[:merchant_id]
-      @account_id               = options[:account_id]
-      @password                 = options[:password]
-      @gateway_url              = options[:gateway_url] || 'https://www.thepay.cz/gate'
-      @value                    = options[:value]
-      @currency                 = options[:currency] || 'CZK'
-      @descritpion              = options[:description]
-      @merchant_data            = options[:merchant_data]
-      @customer_email           = options[:customer_email]
-      @return_url               = options[:return_url]
-      @back_to_eshop_url        = options[:back_to_eshop_url]
+      @merchant_id              = options[:merchant_id]               || 1
+      @account_id               = options[:account_id]                || 3
+      @password                 = options[:password]                  || 'my$up3rsecr3tp4$$word'
+      @gateway_url              = options[:gateway_url]               || 'https://www.thepay.cz/demo-gate'
+      @value                    = options[:value]                     || 120
+      @currency                 = options[:currency]                  || 'CZK'
+      @descritpion              = options[:description]               || 'test description'
+      @merchant_data            = options[:merchant_data]             || 'returned value from gateway'
+      @customer_email           = options[:customer_email]            || 'test@test.com'
+      @return_url               = options[:return_url]                || 'http://localhost:3000'
+      @back_to_eshop_url        = options[:back_to_eshop_url]         || ''
       @merchant_specific_symbol = options[:merchant_specific_symbol]
     end
 
@@ -41,20 +41,20 @@ module ThePay
     private
     def send_request(options = {})
       data = prepare_data(options)
-      connection = Net::HTTP.new(gateway_url, 443)
-      connection.use_ssl = true
 
-      http_response = connection.start do |http|
-        post = Net::HTTP::Post.new(url)
-        post.set_form_data(data)
-        http.request(post)
+      uri = URI('https://www.thepay.cz/demo-gate')
+      req = Net::HTTP::Post.new(uri)
+      req.set_form_data('merchantId' => '1', 'accountId' => '3', 'signature' => signature)
+
+      res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+        http.request(req)
       end
 
-      if http_response.code == '200'
-        response = Response.parse(http_response.body)
-        verify!(response) if response.status == 'OK'
+      if res.code == '200'
+        #response = Response.parse(res.body)
+        #verify!(response) if response.status == 'OK'
 
-        return response
+        return res.body
       else
         # TODO raise RequestFailed
       end
